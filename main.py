6c037e402344c5f28b8e6e35c030251be78b6106
BIngo7 <56383722+287857652@users.noreply.github.com>
2021-01-10 13:25:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from yiban import YiBan
 import util
 if __name__ == '__main__':
     try:
-        yb = YiBan("19974853664", "jxh5116883187") # FIXME:账号密码
+        yb = YiBan("18569630663", "147258369qc") # FIXME:账号密码
         yb.login()
         yb.getHome()
         print("登录成功 %s"%yb.name)
